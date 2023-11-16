b988656e3104ef9d9586869f96807d89fa58ecd1
ashleyakamine <ashley.akamine@plu.edu>
2023-11-15 21:28:04 -0800

    organized file layout & added pages

---
+++ src/pages/InventoryPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function InventoryPage() {
+
+  return (
+    <div>
+      <h1>Welcome to the Inventory Page!</h1>
+    </div>
+  );
+}
---
+++ src/pages/RecipePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function RecipePage() {
+
+  return (
+    <div>
+      <h1>Welcome to the Recipe Page!</h1>
+    </div>
+  );
+}
