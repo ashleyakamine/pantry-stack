c8fc24cfd40c4e373b6984c025cbeb17466f16bb
ashleyakamine <ashley.akamine@plu.edu>
2023-12-07 10:32:24 -0800

    updated database script

--- mySQL/inventory_upload.py
+++ mySQL/inventory_upload.py
@@ -2,63 +2,59 @@ import mysql.connector
 from mysql.connector import errorcode
 
 file = open('Pantry_Inventory.csv', 'r')
-file.readline()   #skip headers line
+file.readline()   # Skip headers line
 rows = []
 for line in file:
-   current = [i for i in line.split(',')]
-   current.pop()
+   current = [i.strip() for i in line.split(',')]  # Added strip() to remove potential whitespace
    rows.append(current)
 file.close()
 
 try:
     reservationConnection = mysql.connector.connect(
         user='root',
-        password= '****', #enter password here
+        password='****',  # Enter password here
         host='localhost',
         database='plu_pantry'
     )
 
-except mysql.connector.Error as err:
-   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-      print('Invalid credentials')
-   elif err.errno == errorcode.ER_BAD_DB_ERROR:
-      print('Database not found')
-   else:
-      print('Cannot connect to database:', err)
+    inventoryCursor = reservationConnection.cursor()
 
-else:
-   inventoryCursor = reservationConnection.cursor()
+    inventoryCreate = ('CREATE TABLE IF NOT EXISTS Inventory ('
+                       'id INT AUTO_INCREMENT PRIMARY KEY,'
+                       'brand VARCHAR(100),'
+                       'simple_name VARCHAR(50),'
+                       'exp_date DATE,'
+                       'quantity TINYINT UNSIGNED,'
+                       'stock_date DATE)')
 
-   inventoryCreate = ('CREATE TABLE Inventory ('
-                'brand VARCHAR(100) PRIMARY KEY,'
-                'simple_name VARCHAR(50),'
-                'exp_date DATE,'
-                'quantity TINYINT UNSIGNED,'
-                'stock_date DATE)')
+    inventoryCursor.execute(inventoryCreate)
+    reservationConnection.commit()
 
-   inventoryCursor.execute(inventoryCreate)
-   reservationConnection.commit()
+    for row in rows:
+        brand = row[0]
+        simple_name = row[1]
+        exp_date = row[2] if row[2] != 'TBD' else None
+        quantity = int(row[3])
 
+        insertQuery = ("INSERT INTO Inventory "
+                       "(brand, simple_name, exp_date, quantity, stock_date) "
+                       "VALUES (%s, %s, %s, %s, CURDATE())")
 
-   for row in rows:
-      brand = row.pop(0)
-      simple_name = row.pop(0)
-      exp_date = row.pop(0)
-      quantity = int(row.pop(0))
-
-      if exp_date == 'TBD':
-         exp_date = None
-
-      insertQuery = ("INSERT INTO Inventory "
-                  "(brand, simple_name, exp_date, quantity, stock_date) "
-                  "VALUES (%s, %s, %s, %s, CURDATE())")
-
-      values = (brand, simple_name, exp_date, quantity)
-      inventoryCursor.execute(insertQuery, values)
-      #print(inventoryCursor.fetchwarnings())   #prints any warnings that occur? idk
-      reservationConnection.commit()
+        values = (brand, simple_name, exp_date, quantity)
+        inventoryCursor.execute(insertQuery, values)
+        reservationConnection.commit()
    
-   inventoryCursor.close()
+    inventoryCursor.close()
+    print('Success')
 
-   print('success')
-   reservationConnection.close()
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('Invalid credentials')
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print('Database not found')
+    else:
+        print('Database error:', err)
+
+finally:
+    if reservationConnection.is_connected():
+        reservationConnection.close()
--- mySQL/recipe_upload.py
+++ mySQL/recipe_upload.py
@@ -2,57 +2,66 @@ import mysql.connector
 from mysql.connector import errorcode
 
 file = open('Recipe_cleanup2.csv', 'r')
-file.readline()   #skip headers line
+file.readline()   # Skip headers line
 rows = []
 
 for line in file:
-   current = [i for i in line.split(',')]
-   current.pop()
-   rows.append(current)
+    current = [i for i in line.split(',')]
+    current.pop()
+    rows.append(current)
 file.close()
 
 try:
     reservationConnection = mysql.connector.connect(
         user='root',
-        password= '***', #enter password here
+        password='***',  # Enter password here
         host='localhost',
         database='plu_pantry'
     )
 
+    recipeCursor = reservationConnection.cursor()
+
+    recipeCreate = ('CREATE TABLE IF NOT EXISTS Recipes ('
+                    'title VARCHAR(200) PRIMARY KEY,'
+                    'link VARCHAR(100),'
+                    'ingredients VARCHAR(1000))')
+
+    recipeCursor.execute(recipeCreate)
+    reservationConnection.commit()
+
+    for row in rows:
+        title = row[0]
+        link = row[1]
+        ingredients = ','.join(row[2:])
+
+        insertQuery = ("INSERT INTO Recipes "
+                       "(title, link, ingredients) "
+                       "VALUES (%s, %s, %s)")
+
+        values = (title, link, ingredients)
+        recipeCursor.execute(insertQuery, values)
+        reservationConnection.commit()
+
+    # Recipe data cleaning
+    updateTitles = "UPDATE Recipes SET title = REPLACE(REPLACE(title, '\"', ''), '*', '')"
+    recipeCursor.execute(updateTitles)
+    reservationConnection.commit()
+
+    updateIngredients = "UPDATE Recipes SET ingredients = REPLACE(REPLACE(ingredients, '\"', ''), '*', '')"
+    recipeCursor.execute(updateIngredients)
+    reservationConnection.commit()
+
+    recipeCursor.close()
+    print('Success')
+
 except mysql.connector.Error as err:
-   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-      print('Invalid credentials')
-   elif err.errno == errorcode.ER_BAD_DB_ERROR:
-      print('Database not found')
-   else:
-      print('Cannot connect to database:', err)
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('Invalid credentials')
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print('Database not found')
+    else:
+        print('Cannot connect to database:', err)
 
-else:
-   recipeCursor = reservationConnection.cursor()
-
-   recipeCreate = ('CREATE TABLE Recipes ('
-                'title VARCHAR(200) PRIMARY KEY,'
-                'link VARCHAR(100),'
-                'ingredients VARCHAR(1000))')
-
-   recipeCursor.execute(recipeCreate)
-   reservationConnection.commit()
-
-
-   for row in rows:
-      title = row.pop(0)
-      link = row.pop(0)
-      ingredients = ','.join(row)
-
-      insertTestQuery = ("INSERT INTO Recipes "
-                  "(title, link, ingredients) "
-                  "VALUES (%s, %s, %s)")
-
-      values = (title, link, ingredients)
-      recipeCursor.execute(insertTestQuery, values)
-      reservationConnection.commit()
-   
-   recipeCursor.close()
-
-   print('success')
-   reservationConnection.close()
+finally:
+    if reservationConnection.is_connected():
+        reservationConnection.close()
